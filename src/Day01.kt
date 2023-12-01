6544c655d6bb87faa60698ba3b41bb4046c67419
Xiaoyuan Wang <xiaoyuanwang90@gmail.com>
2023-12-01 10:55:34 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,51 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { s -> s.filter { it.isDigit() } }
+            .sumOf { "${it.first()}${it.last()}".toInt() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        val str2Int = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9,
+        )
+
+        fun String.firstDigit(): Int {
+            for (i in this.indices) {
+                if (this[i].isDigit()) return "${this[i]}".toInt()
+
+                for ((k, v) in str2Int) {
+                    if (this.startsWith(k, i)) return v
+                }
+            }
+            return 0
+        }
+
+        fun String.lastDigit(): Int {
+            for (i in this.indices.reversed()) {
+                if (this[i].isDigit()) return "${this[i]}".toInt()
+
+                for ((k, v) in str2Int) {
+                    if (this.startsWith(k, i)) return v
+                }
+            }
+            return 0
+        }
+
+        return input.sumOf { it.firstDigit() * 10 + it.lastDigit() }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
